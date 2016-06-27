ba167e9b36d6e29894d01faff6369500d9ac60e5
lklein <lacusklein2013@gmail.com>
2016-06-26 23:51:26 -0700

    regenerated karma.conf.js

--- karma.conf.js
+++ karma.conf.js
@@ -1,46 +1,69 @@
-'use strict';
-
 // Karma configuration
+// Generated on Sat Jun 25 2016 23:49:12 GMT-0700 (Pacific Daylight Time)
+
 module.exports = function(config) {
   config.set({
-    // Frameworks to use
+
+    // base path that will be used to resolve all patterns (eg. files, exclude)
+    basePath: '',
+
+
+    // frameworks to use
+    // available frameworks: https://npmjs.org/browse/keyword/karma-adapter
     frameworks: ['jasmine'],
 
-    basePath: 'app/',
 
+    // list of files / patterns to load in the browser
     files: [
-        '*.spec.js'
-    ], 
+      'app/*.js'
+    ],
 
-    // Test results reporter to use
-    // Possible values: 'dots', 'progress', 'junit', 'growl', 'coverage'
-    // reporters: ['progress'],
+
+    // list of files to exclude
+    exclude: [
+    ],
+
+
+    // preprocess matching files before serving them to the browser
+    // available preprocessors: https://npmjs.org/browse/keyword/karma-preprocessor
+    preprocessors: {
+    },
+
+
+    // test results reporter to use
+    // possible values: 'dots', 'progress'
+    // available reporters: https://npmjs.org/browse/keyword/karma-reporter
     reporters: ['progress'],
 
-    // Web server port
+
+    // web server port
     port: 9876,
 
-    // Enable / disable colors in the output (reporters and logs)
+
+    // enable / disable colors in the output (reporters and logs)
     colors: true,
 
-    // Enable / disable watching file and executing tests whenever any file changes
+
+    // level of logging
+    // possible values: config.LOG_DISABLE || config.LOG_ERROR || config.LOG_WARN || config.LOG_INFO || config.LOG_DEBUG
+    logLevel: config.LOG_INFO,
+
+
+    // enable / disable watching file and executing tests whenever any file changes
     autoWatch: true,
 
-    // Start these browsers, currently available:
-    // - Chrome
-    // - ChromeCanary
-    // - Firefox
-    // - Opera
-    // - Safari (only Mac)
-    // - PhantomJS
-    // - IE (only Windows)
+
+    // start these browsers
+    // available browser launchers: https://npmjs.org/browse/keyword/karma-launcher
     browsers: ['PhantomJS'],
 
-    // If browser does not capture in given timeout [ms], kill it
-    captureTimeout: 60000,
 
     // Continuous Integration mode
-    // If true, it capture browsers, run tests and exit
-    singleRun: false
-  });
-};
+    // if true, Karma captures browsers, runs the tests and exits
+    singleRun: false,
+
+    // Concurrency level
+    // how many browser should be started simultaneous
+    concurrency: Infinity
+  })
+}
